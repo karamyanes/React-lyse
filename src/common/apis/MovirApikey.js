90693b2d352583a12b1280f39bf285dc759dad13
karamyanes <karamyanes@hotmail.com>
2023-04-18 15:52:40 +0200

    first commit

---
+++ src/common/apis/MovirApikey.js
@@ -0,0 +1,7 @@
+export const APIKey = "bd6752ea";
+//Please append it to all of your API requests,
+
+//OMDb API: http: //www.omdbapi.com/?i=tt3896198&apikey=bd6752ea
+
+//  Click the following URL to activate your key: http: //www.omdbapi.com/apikey.aspx?VERIFYKEY=237c78f2-2823-46e9-a24d-81264d685ab5
+//  If you did not make this request, please disregard this email.
